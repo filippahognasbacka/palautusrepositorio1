75f43300cf15d0fdd2d71e5f28a85b0eb9e0898e
Filippa Högnäsbacka <filippa.hognasbacka@gmail.com>
2024-11-09 21:19:54 +0200

    laitettu index kuntoon

--- viikko2/nhl-reader/src/index.py
+++ viikko2/nhl-reader/src/index.py
@@ -1,7 +1,18 @@
-from player import Player
+from player_reader import PlayerReader
+from player_stats import PlayerStats
 
 def main():
-    pass
+    url = "https://studies.cs.helsinki.fi/nhlstats/"
+    season = input("Select season ")
+    nationality = input("Select nationality ")
+    
+    
+    reader = PlayerReader(url)
+    stats = PlayerStats(reader)
+    players = stats.top_scorers_by_nationality(nationality, season)
+
+    print(f"Top scorers from {nationality} in {season}: ")
+    stats.display_players(players)
 
 if __name__ == "__main__":
     main()
