0d2661b99079f0d8559b3c3b93a036cf9bed8bc5
Filippa Högnäsbacka <filippa.hognasbacka@gmail.com>
2024-12-01 19:36:48 +0200

    tehtävä3

--- viikko6/query-language/src/matchers.py
+++ viikko6/query-language/src/matchers.py
@@ -53,3 +53,12 @@ class HasFewerThan:
 
 
 
+class Or:
+    def __init__(self, *matchers):
+        self._matchers = matchers
+
+    def test(self, player):
+        for matcher in self._matchers:
+            if matcher.test(player):
+                return True
+        return False
